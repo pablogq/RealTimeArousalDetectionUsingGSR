f9d608dbb4ed9de537a9adf5f71bfd87f5dabcd0
Dessislava Vassileva <ddessy@gmail.com>
2016-02-18 11:42:36 +0100

    add a project for displaying.

---
+++ DisplayGSRSignal/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DisplayGSRSignal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 484);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Display";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chart1.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.Unscaled;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "GSR Channel 1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series1.YValuesPerPoint = 20;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1600, 454);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1514, 522);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "GSRSignal";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ DisplayGSRSignal/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.Rage.GSRAsset;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
+using System.Linq;
+
+namespace DisplayGSRSignal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChartClean();
+            GSRSignalProcessor gsrHandler = new GSRSignalProcessor();
+            ChartArea chart = chart1.ChartAreas[0];
+            ///*
+            Dictionary<int, List<int>> channelsValues = gsrHandler.ExtractChannelsValues();
+            gsrHandler.FillCoordinates();
+
+            int blockSize = 100;
+            double maxValue = 0.0;
+            double minValue = 0.0;
+            int numPoints = 0;
+            foreach (KeyValuePair<int, Dictionary<double, int>> channelCoordinates in gsrHandler.coordinates)
+            {
+                foreach (KeyValuePair<double, int> coordinate in channelCoordinates.Value)
+                {
+                    if(numPoints < 4700)
+                    {
+                        if (maxValue.Equals(0.0)) maxValue = coordinate.Key;
+                        Log(maxValue);
+                        minValue = coordinate.Key;
+                        Log("minValue: " + minValue);
+                        chart1.Series["GSR Channel 1"].Points.AddXY(coordinate.Key, coordinate.Value);
+                    }
+                    numPoints++;
+                }
+                
+            }
+
+            chart1.Series["GSR Channel 1"].ChartType = SeriesChartType.Spline;
+
+            chart.AxisX.Title = "Time";
+            chart.AxisY.Title = "GSR";
+
+            // Set maximum values.
+            chart.AxisX.Maximum = maxValue;
+
+            // enable autoscroll
+            chart.CursorX.AutoScroll = true;
+
+            // let's zoom to [0,blockSize] (e.g. [0,100])
+            chart.AxisX.ScaleView.Zoomable = true;
+            chart.AxisX.ScaleView.SizeType = DateTimeIntervalType.Number;
+
+            chart.AxisX.ScaleView.Zoom(minValue, minValue+800);
+
+            // disable zoom-reset button (only scrollbar's arrows are available)
+            chart.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll;
+
+            // set scrollbar small change 
+            chart.AxisX.ScaleView.SmallScrollSize = 800;
+            //*/
+            /*
+
+            int blockSize = 100;
+
+            // generates random data (i.e. 30 * blockSize random numbers)
+            Random rand = new Random();
+            var valuesArray = Enumerable.Range(0, blockSize * 30).Select(x => rand.Next(1, 10)).ToArray();
+
+            // clear the chart
+            chart1.Series.Clear();
+
+            // fill the chart
+            var series = chart1.Series.Add("My Series");
+            series.ChartType = SeriesChartType.Line;
+            series.XValueType = ChartValueType.Int32;
+            for (int i = 0; i < valuesArray.Length; i++)
+                series.Points.AddXY(i, valuesArray[i]);
+            var chartArea = chart1.ChartAreas[series.ChartArea];
+
+            // set view range to [0,max]
+            chartArea.AxisX.Minimum = 0.0;
+            chartArea.AxisX.Maximum = 5000.20;
+
+            // enable autoscroll
+            chartArea.CursorX.AutoScroll = true;
+
+            // let's zoom to [0,blockSize] (e.g. [0,100])
+            chartArea.AxisX.ScaleView.Zoomable = true;
+            chartArea.AxisX.ScaleView.SizeType = DateTimeIntervalType.Number;
+            int position = 0;
+            int size = blockSize;
+            chartArea.AxisX.ScaleView.Zoom(position, 1000);
+
+            // disable zoom-reset button (only scrollbar's arrows are available)
+            chartArea.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll;
+
+            // set scrollbar small change to blockSize (e.g. 100)
+            chartArea.AxisX.ScaleView.SmallScrollSize = blockSize;
+            */
+
+        }
+
+        private void ChartClean()
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+        }
+
+        public static void Log(object logMessage)
+        {
+            using (StreamWriter w = File.AppendText("c:\\Users\\ddessy\\log.txt"))
+            {
+                logMessage = logMessage.ToString();
+                w.Write("\r\nLog Entry : ");
+                w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                    DateTime.Now.ToLongDateString());
+                w.WriteLine("  :");
+                w.WriteLine("  :{0}", logMessage);
+                w.WriteLine("-------------------------------");
+            }
+        }
+    }
+}
