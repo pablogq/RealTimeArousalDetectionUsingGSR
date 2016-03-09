2a2b5b85a62c1490de5cfedf4631d14fa94575af
Dessislava Vassileva <ddessy@gmail.com>
2016-03-09 16:15:37 +0100

    Initial implementation of multiple channels view

--- DisplayGSRSignal/Form1.cs
+++ DisplayGSRSignal/Form1.cs
@@ -4,27 +4,40 @@ using Assets.Rage.GSRAsset;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using System.IO;
-using System.Linq;
+using System.Configuration;
+using System.Drawing;
 
 namespace DisplayGSRSignal
 {
     public partial class Form1 : Form
     {
+        int number;
+
         public Form1()
         {
             InitializeComponent();
+            number = 1;
+            threeIn1.Checked = true;
+            chart1.Visible = false;
+            GSRChartDisplay();
+            timer1.Tick += timer1_Tick;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            timer1.Start();
+            GSRChartDisplay();
+        }
+
+        private void GSRChartDisplay()
+        {
             ChartClean();
             GSRSignalProcessor gsrHandler = new GSRSignalProcessor();
-            ChartArea chart = chart1.ChartAreas[0];
-            ///*
+            ChartArea chart = gsrChart.ChartAreas[0];
+
             Dictionary<int, List<int>> channelsValues = gsrHandler.ExtractChannelsValues();
             gsrHandler.FillCoordinates();
 
-            int blockSize = 100;
             double maxValue = 0.0;
             double minValue = 0.0;
             int numPoints = 0;
@@ -32,20 +45,17 @@ namespace DisplayGSRSignal
             {
                 foreach (KeyValuePair<double, int> coordinate in channelCoordinates.Value)
                 {
-                    if(numPoints < 4700)
+                    //numPoints > (number - 1) * 5000 && numPoints < number*5000
+                    if (numPoints < number*5000)
                     {
                         if (maxValue.Equals(0.0)) maxValue = coordinate.Key;
-                        Log(maxValue);
                         minValue = coordinate.Key;
-                        Log("minValue: " + minValue);
-                        chart1.Series["GSR Channel 1"].Points.AddXY(coordinate.Key, coordinate.Value);
+                        gsrChart.Series["Channel 1"].Points.AddXY(coordinate.Key, coordinate.Value);
                     }
                     numPoints++;
                 }
-                
-            }
 
-            chart1.Series["GSR Channel 1"].ChartType = SeriesChartType.Spline;
+            }
 
             chart.AxisX.Title = "Time";
             chart.AxisY.Title = "GSR";
@@ -56,63 +66,42 @@ namespace DisplayGSRSignal
             // enable autoscroll
             chart.CursorX.AutoScroll = true;
 
-            // let's zoom to [0,blockSize] (e.g. [0,100])
+            //zoom to [minValue, minvalue+numberOfVisiblePoints]
             chart.AxisX.ScaleView.Zoomable = true;
             chart.AxisX.ScaleView.SizeType = DateTimeIntervalType.Number;
 
-            chart.AxisX.ScaleView.Zoom(minValue, minValue+800);
+            chart.AxisX.ScaleView.Zoom(minValue, minValue + 800);
 
             // disable zoom-reset button (only scrollbar's arrows are available)
             chart.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll;
 
             // set scrollbar small change 
             chart.AxisX.ScaleView.SmallScrollSize = 800;
-            //*/
-            /*
 
-            int blockSize = 100;
+            DisplayFakeSeriesForTest(minValue);
+        }
 
-            // generates random data (i.e. 30 * blockSize random numbers)
-            Random rand = new Random();
-            var valuesArray = Enumerable.Range(0, blockSize * 30).Select(x => rand.Next(1, 10)).ToArray();
+        private void DisplayFakeSeriesForTest(double minValue)
+        {
+            Random rdn = new Random();
+            for (int i = 0; i < 4700; i++)
+            {
+                gsrChart.Series["Channel 2"].Points.AddXY
+                                (minValue + i * 20, rdn.Next(1, 8));
+                gsrChart.Series["Channel 3"].Points.AddXY
+                                (minValue + i * 10, rdn.Next(1, 8));
+            }
 
-            // clear the chart
-            chart1.Series.Clear();
+            gsrChart.Series["Channel 2"].Color = Color.Green;
+            gsrChart.Series["Channel 3"].Color = Color.Magenta;
 
-            // fill the chart
-            var series = chart1.Series.Add("My Series");
-            series.ChartType = SeriesChartType.Line;
-            series.XValueType = ChartValueType.Int32;
-            for (int i = 0; i < valuesArray.Length; i++)
-                series.Points.AddXY(i, valuesArray[i]);
-            var chartArea = chart1.ChartAreas[series.ChartArea];
-
-            // set view range to [0,max]
-            chartArea.AxisX.Minimum = 0.0;
-            chartArea.AxisX.Maximum = 5000.20;
-
-            // enable autoscroll
-            chartArea.CursorX.AutoScroll = true;
-
-            // let's zoom to [0,blockSize] (e.g. [0,100])
-            chartArea.AxisX.ScaleView.Zoomable = true;
-            chartArea.AxisX.ScaleView.SizeType = DateTimeIntervalType.Number;
-            int position = 0;
-            int size = blockSize;
-            chartArea.AxisX.ScaleView.Zoom(position, 1000);
-
-            // disable zoom-reset button (only scrollbar's arrows are available)
-            chartArea.AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll;
-
-            // set scrollbar small change to blockSize (e.g. 100)
-            chartArea.AxisX.ScaleView.SmallScrollSize = blockSize;
-            */
-
+            //gsrChart.Series["Channel 2"].IsVisibleInLegend = false;
+            //gsrChart.Series["Channel 3"].IsVisibleInLegend = false;
         }
 
         private void ChartClean()
         {
-            foreach (var series in chart1.Series)
+            foreach (var series in gsrChart.Series)
             {
                 series.Points.Clear();
             }
@@ -120,16 +109,51 @@ namespace DisplayGSRSignal
 
         public static void Log(object logMessage)
         {
-            using (StreamWriter w = File.AppendText("c:\\Users\\ddessy\\log.txt"))
+            String currentDate = DateTime.Now.ToString("yyyyMMdd");
+            String logFileName = ConfigurationManager.AppSettings.Get("LogFile").Replace(".txt", currentDate + ".txt");
+            using (StreamWriter w = File.AppendText(logFileName))
             {
                 logMessage = logMessage.ToString();
                 w.Write("\r\nLog Entry : ");
-                w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                w.Write("{0} {1}", DateTime.Now.ToLongTimeString(),
                     DateTime.Now.ToLongDateString());
-                w.WriteLine("  :");
                 w.WriteLine("  :{0}", logMessage);
                 w.WriteLine("-------------------------------");
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            number++;
+            GSRChartDisplay();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void threeIn3_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Visible = true;
+            chart1.Series.Add(gsrChart.Series["Channel 2"]);
+            gsrChart.Series.Remove(gsrChart.Series["Channel 2"]);
+        }
+
+        private void threeIn1_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                if ("Channel 2".Equals(series.Name))
+                {
+                    gsrChart.Series["Channel 2"] = chart1.Series["Channel 2"];
+                    chart1.Series.Remove(gsrChart.Series["Channel 2"]);
+                }
+            }
+
+
+            chart1.Visible = false;
+            chart1.Series["yyyy"].IsVisibleInLegend = false;
         }
     }
 }
