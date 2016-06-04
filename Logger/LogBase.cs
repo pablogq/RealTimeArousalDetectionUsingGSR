88d3f653bc502c85dd81ac809ce1bb50e19a8fac
Dessislava Vassileva <ddessy@gmail.com>
2016-06-04 20:49:15 +0200

    Add logger.

---
+++ Logger/LogBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public abstract class LogBase
+
+    {
+        protected readonly object lockObj = new object();
+        public abstract void Log(string message);
+
+    }
