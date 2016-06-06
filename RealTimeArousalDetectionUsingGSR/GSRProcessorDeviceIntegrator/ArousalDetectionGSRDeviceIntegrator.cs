dcf27258acf6ce0353d1d985b98f6d00e0787bb0
Dessislava Vassileva <ddessy@gmail.com>
2016-06-06 04:57:45 +0200

    Add unit tests

---
+++ RealTimeArousalDetectionUsingGSR/GSRProcessorDeviceIntegrator/ArousalDetectionGSRDeviceIntegrator.cs
@@ -0,0 +1,120 @@
+﻿using Assets.Rage.GSRAsset.SignalDevice;
+using Assets.Rage.GSRAsset.SignalProcessor;
+using System;
+
+namespace Assets.Rage.GSRAsset.Integrator
+{
+    public class ArousalDetectionGSRDeviceIntegrator : IArousalDetection, ISignalDeviceController
+    {
+        GSRHRDevice gsrDevice;
+        GSRSignalProcessor gsrProcessor;
+
+        public ArousalDetectionGSRDeviceIntegrator()
+        {
+            gsrDevice = new GSRHRDevice();
+            gsrProcessor = new GSRSignalProcessor();
+        }
+
+        public GSRHRDevice GetGSRDevice()
+        {
+            return gsrDevice;
+        }
+
+        public GSRSignalProcessor GetGSRSignalProcessor()
+        {
+            return gsrProcessor;
+        }
+
+        public double GetGSRFeature(string featureName)
+        {
+            if ("SCRArousalArea".Equals(featureName))
+            {
+                return gsrProcessor.GetArousalStatistics().SCRArousalArea;
+            }
+
+            if ("SCRAchievedArousalLevel".Equals(featureName))
+            {
+                return gsrProcessor.GetArousalStatistics().SCRAchievedArousalLevel;
+            }
+
+            if ("SCLAchievedArousalLevel".Equals(featureName))
+            {
+                return gsrProcessor.GetArousalStatistics().SCLAchievedArousalLevel;
+            }
+
+            if ("MovingAverage".Equals(featureName))
+            {
+                return gsrProcessor.GetArousalStatistics().MovingAverage;
+            }
+
+            return -1;
+        }
+
+        public void GetSignalData(byte[] data)
+        {
+            gsrDevice.GetSignalData(data);
+        }
+
+        public int GetSignalSampleRate()
+        {
+            return gsrDevice.GetSignalSampleRate();
+        }
+
+        public void OpenPort()
+        {
+            gsrDevice.OpenPort();
+        }
+
+        public void SelectCOMPort(string portName)
+        {
+            gsrDevice.SelectCOMPort(portName);
+        }
+
+        public int SetMaxArousalLevel(int numberOfLevels)
+        {
+            if (numberOfLevels < 100 && numberOfLevels > 0)
+            {
+                gsrProcessor.ArousalLevel = numberOfLevels;
+                return 0;
+            }
+
+            return -1;
+        }
+
+        public void SetSignalSamplerate()
+        {
+            gsrDevice.SetSignalSamplerate();
+        }
+
+        public int SetSignalSamplerate(string speed)
+        {
+            return gsrDevice.SetSignalSamplerate(speed);
+        }
+
+        public void SetSignalSettings()
+        {
+            gsrDevice.SetSignalSettings();
+        }
+
+        public int SetTimeWindow(double timeWindow)
+        {
+            if (timeWindow > 0)
+            {
+                gsrProcessor.DefaultTimeWindow = Convert.ToDouble(timeWindow);
+                return 0;
+            }
+
+            return -1;
+        }
+
+        public int StartSignalsRecord()
+        {
+            return gsrDevice.StartSignalsRecord();
+        }
+
+        public int StopSignalsRecord()
+        {
+            return gsrDevice.StartSignalsRecord();
+        }
+    }
+}
---
+++ RealTimeArousalDetectionUsingGSR/UnitTestArousalDetectionByGSRDevice/GSRSignalProcessorSettings.cs
@@ -0,0 +1,63 @@
+﻿using Assets.Rage.GSRAsset.Integrator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assets.Rage.GSRAsset.UnitTest
+{
+    [TestClass]
+    public class GSRSignalProcessorSettings
+    {
+        [TestMethod]
+        public void SetMaxArousalLevel_SetNewValue_ReturnTrue()
+        {
+            //arrange
+            ArousalDetectionGSRDeviceIntegrator gsrHandler = new ArousalDetectionGSRDeviceIntegrator();
+
+            //act
+            int newMaxArousalLevel = gsrHandler.SetMaxArousalLevel(2);
+
+            //assert
+            Assert.AreEqual(gsrHandler.GetGSRSignalProcessor().ArousalLevel, 2);
+        }
+
+        [TestMethod]
+        public void SetMaxArousalLevel_SetNewValue_ReturnFalse()
+        {
+            //arrange
+            ArousalDetectionGSRDeviceIntegrator gsrHandler = new ArousalDetectionGSRDeviceIntegrator();
+            int oldArousalLevel = gsrHandler.GetGSRSignalProcessor().ArousalLevel;
+
+            //act
+            int newMaxArousalLevel = gsrHandler.SetMaxArousalLevel(oldArousalLevel + 1);
+
+            //assert
+            Assert.AreNotEqual(gsrHandler.GetGSRSignalProcessor().ArousalLevel, oldArousalLevel);
+        }
+
+        [TestMethod]
+        public void SetTimeWindow_SetNewValue_ReturnTrue()
+        {
+            //arrange
+            ArousalDetectionGSRDeviceIntegrator gsrHandler = new ArousalDetectionGSRDeviceIntegrator();
+
+            //act
+            int newMaxArousalLevel = gsrHandler.SetTimeWindow(15);
+
+            //assert
+            Assert.AreEqual(gsrHandler.GetGSRSignalProcessor().DefaultTimeWindow, 15);
+        }
+
+        [TestMethod]
+        public void SetTimeWindow_SetNewValue_ReturnFalse()
+        {
+            //arrange
+            ArousalDetectionGSRDeviceIntegrator gsrHandler = new ArousalDetectionGSRDeviceIntegrator();
+            double oldTimeWindow = gsrHandler.GetGSRSignalProcessor().DefaultTimeWindow;
+
+            //act
+            int newMaxArousalLevel = gsrHandler.SetTimeWindow(oldTimeWindow + 10);
+
+            //assert
+            Assert.AreNotEqual(gsrHandler.GetGSRSignalProcessor().DefaultTimeWindow, oldTimeWindow);
+        }
+    }
+}
