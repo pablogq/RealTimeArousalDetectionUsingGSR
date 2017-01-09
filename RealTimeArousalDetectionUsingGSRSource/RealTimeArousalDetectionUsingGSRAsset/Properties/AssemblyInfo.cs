8e2f8861abc146f01ee8d817e2f1d8fae7cc4eff
Dessislava Vassileva <ddessy@gmail.com>
2017-01-09 04:15:32 +0100

    change name of the class GSRProcessorDeviceIntegrator.

--- RealTimeArousalDetectionUsingGSRSource/RealTimeArousalDetectionUsingGSRAsset/Properties/AssemblyInfo.cs
+++ RealTimeArousalDetectionUsingGSRSource/RealTimeArousalDetectionUsingGSRAsset/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GSRProcessorDeviceIntegrator")]
+[assembly: AssemblyTitle("RealTimeArousalDetectionUsingGSRAsset")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sofia University")]
-[assembly: AssemblyProduct("GSRProcessorDeviceIntegrator")]
+[assembly: AssemblyProduct("RealTimeArousalDetectionUsingGSRAsset")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
